97ead0a9e574e31504575d00790d051d8b47bf13
sdamaggio <stefano.damaggio@gmail.com>
2015-04-01 11:14:30 +0200

    joystick working but only on edge detection

--- pivaders/pivaders.py
+++ pivaders/pivaders.py
@@ -15,6 +15,46 @@ MISSILE_SIZE = (5, 5)
 BLOCK_SIZE = (10, 10)
 RES = (800, 600)
 
+rightPressed = False
+leftPressed = False
+
+# http://raspi.tv/how-to-use-interrupts-with-python-on-the-raspberry-pi-and-rpi-gpio-part-2
+import RPi.GPIO as GPIO
+GPIO.setmode(GPIO.BCM)
+
+# Joystick:
+# UP            gpio22  pin 15
+# DOWN          gpio17  pin 11
+# RIGHT         gpio27  pin 13
+# LEFT          gpio18  pin 12
+# BUTTON1       gpio23  pin 16
+# GND           pin 11, 14
+
+GPIO.setup(27, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+GPIO.setup(18, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+GPIO.setup(23, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+
+# now we'll define the threaded callback function
+# this will run in another thread when our event is detected    
+def callbackRIGHT(channel):
+    global rightPressed
+    rightPressed = True
+
+def callbackLEFT(channel):
+    global leftPressed
+    leftPressed = True
+
+def callbackBUTTON1(channel):
+    print "SHOOT\n"
+
+# The GPIO.add_event_detect() line below set things up so that
+# when a rising edge is detected on the pin, regardless of whatever
+# else is happening in the program, the function "callbackXXXX" will be run
+# It will happen even while the program is waiting for
+# a falling edge on the other button.
+GPIO.add_event_detect(27, GPIO.FALLING, callback=callbackRIGHT, bouncetime=300)
+GPIO.add_event_detect(18, GPIO.FALLING, callback=callbackLEFT, bouncetime=300)
+GPIO.add_event_detect(23, GPIO.FALLING, callback=callbackBUTTON1, bouncetime=300)
 
 class Player(pygame.sprite.Sprite):
     def __init__(self):
@@ -161,14 +201,26 @@ class Game(object):
                 else:
                     GameState.start_screen = True
         self.keys = pygame.key.get_pressed()
-        if self.keys[pygame.K_LEFT]:
+	global leftPressed
+	global rightPressed
+	if self.keys[pygame.K_LEFT]:
             GameState.vector = -1
             self.animate_left = True
-            self.animate_right = False
+            self.animate_right = False            
         elif self.keys[pygame.K_RIGHT]:
             GameState.vector = 1
             self.animate_right = True
             self.animate_left = False
+	elif leftPressed:
+            GameState.vector = -1
+            self.animate_left = True
+            self.animate_right = False
+            leftPressed = False
+        elif  rightPressed:
+            GameState.vector = 1
+            self.animate_right = True
+            self.animate_left = False
+            rightPressed = False
 
         else:
             GameState.vector = 0
